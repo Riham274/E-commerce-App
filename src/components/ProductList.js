3ea3fe0c7d89f2e2cbce7c248b5b73386e4da3e3
Riham274 <riham.ararawi274@gmail.com>
2025-09-23 17:41:05 +0300

    Add category filtering with API integration

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,16 +4,54 @@ import Product from "./Product";
 function ProductList() {
   const api_url = "https://fakestoreapi.com/products";
   const [products, setProducts] = useState([]);
-
-  useEffect(() => {
+  const [categories, setCategories] = useState([]);
+  const getProducts = () => {
     fetch(api_url)
       .then((response) => response.json())
       .then((data) => setProducts(data));
+  };
+  const getCategories = () => {
+    fetch(`${api_url}/categories`)
+      .then((response) => response.json())
+      .then((data) => setCategories(data));
+  };
+
+  useEffect(() => {
+    getProducts();
+    getCategories();
   }, []);
+
+  const getProductInCategory = (catName) => {
+    fetch(`${api_url}/category/${catName}`)
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  };
+
   return (
     <>
       <h2 className="text-center">Our Products</h2>
       <div className="container">
+        <button
+          className="btn btn-info"
+          onClick={() => {
+            getProducts();
+          }}
+        >
+          All
+        </button>
+        {categories.map((cat) => {
+          return (
+            <button
+              key={cat}
+              className="btn btn-info"
+              onClick={() => {
+                getProductInCategory(cat);
+              }}
+            >
+              {cat}
+            </button>
+          );
+        })}
         <div className="row">
           {products.map((product) => {
             return (
